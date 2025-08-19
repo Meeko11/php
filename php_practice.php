f9c60ebcf896a5f679ccd1dd2eef2db86e724f4d
Meeko11 <162523217+Meeko11@users.noreply.github.com>
2025-08-19 22:20:11 +1000

    Add files via upload

---
+++ php_practice.php
@@ -0,0 +1,127 @@
+HELLO WORLD!
+<!-- 
+<h1>HELLO WORLD!</h1>
+
+<style>
+    h1 {
+
+        color : blue ;
+    }
+ </style> -->
+
+ <script>
+alert("warning ")
+
+
+ </script>
+
+ <?php
+
+    // echo "hilong hilo nako ";
+      #echo "hilong hilo nako ";
+      #
+    /* echo "hilong hilo nako*/
+       echo "hilong hilo nako"
+ ?>   
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1> this is my first php page </h1> -->
+    <?php
+     Echo "Hello world ";
+      //echo "Hello world "
+        //EcHo "Hello world ";
+        
+        //variable
+        // $a = 5;
+       
+        // var_dump($a);
+        // var_dump("okim");
+        // var_dump(3.14);
+        // var_dump(true);
+        // var_dump([1,2,3]);
+        // var_dump(null);
+
+        //variable scope
+
+        //   $a = 5;
+
+        //   function test()
+        //   {
+        //     static $a = 6;
+        //      echo $a;
+        //      echo "<br>";
+        //      $a++;
+        //   }
+        //     // test(); // 6 
+        //     // test();
+        //     // echo $a;
+        //     test();  // 6 
+        //     test();  // 7 
+
+        // echo & print
+        // echo"<br>";
+        // echo("behh <br>"),("bb! <br>");
+        // echo"<br>";
+        // echo"HI <br>","ate";
+        // echo"<br>";
+        // print("hello behh <br>");
+        //  print" behh  <br>";
+        //  echo print("hi");
+
+        // DATA TYPE 
+        //   $a = "willie"; // string
+        //   $b = 5;// int 
+        //   $c = 3.14;// float 
+        //   $d = true;// boolean
+        //   $e =[1,2,3];// array 
+        //   $f = new stdClass();// object
+        //   $g = null; // null 
+        //   $h = fopen("php_practice.php ", "r");
+
+        //   var_dump($a);
+        //     echo"<br>";
+        //   var_dump($b);
+        //      echo"<br>";
+        //   var_dump($c);
+        //     echo"<br>";
+        //   var_dump($d);
+        //     echo"<br>";
+        //   var_dump($e);
+        //     echo"<br>";
+        //   var_dump($f);
+        //     echo"<br>";
+        //   var_dump($g);
+        //     echo"<br>";
+        //   var_dump($h);
+        //     echo"<br>";
+     
+        // php string 
+        //  echo"<br>";
+        //  $a = "violet ";
+        // $name = "<h1 style=\"color:$a\"> banana " .$a;
+        // $name = "banana mo maliit ";
+        // echo"<br>"; 
+        // echo strlen($name),"<br>";
+        // echo str_word_count($name),"<br>";
+        // echo strpos($name,"nana"),"<br>";
+        // echo strtoupper($name),"<br>";
+        // echo strtolower($name),"<br>";
+        // echo str_replace("ban","lit",$name),"<br>";
+        // echo strrev($name),"<br>";
+        // echo trim($name),"<br>";
+        // echo print_r(explode("a",$name),"<br>");
+        
+
+
+
+
+    ?>
+</body>
+</html>
